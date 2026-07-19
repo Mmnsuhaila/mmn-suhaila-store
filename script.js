02203c2f6ab73c80bc244e25c4c53550b17a8a7f
Mmnsuhaila <aishamusabala39@gmail.com>
2026-07-20 00:21:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,17 @@ function checkout() {
         "_blank"
     );
 }
+function searchProducts() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let name = card.querySelector("h3").textContent.toLowerCase();
+
+    if (name.includes(input)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
